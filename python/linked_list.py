67942fb74a83cbc4022bd18722f554dd86b87734
digiwhale <digiwhale.humphrey@gmail.com>
2020-10-22 16:58:25 -0400

    Jason and Andrew

--- python/linked_list.py
+++ python/linked_list.py
@@ -1,19 +1,86 @@
 class LinkList:
   # write your __init__ method here that should store a 'head' value which the first Node in the LinkedList and a 'length' value which is the total number of Nodes in the LinkedList
+  def __init__(self, head=None):
+    self.head = head
+    self.length = 0 
 
   def add(self, data):
     # write your code to ADD an element to the Linked List
-    pass
+    # Pseudocode
+    # Make a new node
+    new_node = Node(data)
+    # Attach to end of list
+    current = self.head
+    if self.head == None:
+      self.head = new_node
+      self.length += 1
+      return
+    while current.next != None:
+      # Starting at self.head, make a variable that continues on each next, until we reach the end (next = None)
+      current = current.next
+      # Set next = our new node
+    current.next = new_node
+    # Add 1 to length
+    self.length += 1
 
   def remove(self, data):
     # write your code to REMOVE an element from the Linked List
-    pass
+    if self.head == None:
+      return
+    # If our head matches the input data value,
+    if self.head.data == data:
+      # Remove the head, keeping track of head's next value
+      # Set our head to the previous head's next value
+      self.head = self.head.next
+      # Length -= 1
+      self.length -= 1
+      
+    # Else, we are going to remove something after the head  
+    current = self.head.next
+    previous = self.head
+    while current != None:
+      if current.data == data:
+        previous.next = current.next
+        self.length -= 1
+        return
+      previous = current
+      current = current.next
+    
 
   def get(self, element_to_get):
     # write you code to GET and return an element from the Linked List
-    pass
+    current = self.head
+    while current != None:
+      if current.data == element_to_get:
+        print(current)
+      current = current.next
+
+  def display(self):
+    """Return a string of all values separated by commas"""
+    output = ''
+    current = self.head
+    while current != None:
+      output += str(current.data) + ', '
+      current = current.next
+    print(output)
 
 # ----- Node ------
 class Node:
   # store your DATA and NEXT values here
-  pass
+  def __init__(self, data=None):
+    self.data = data
+    self.next = None
+    
+ll = LinkList()
+ll.display()
+ll.add(4)
+ll.display()
+ll.add(7)
+ll.display()
+ll.add(2)
+ll.get(2)
+ll.display()
+ll.remove(7)
+ll.display()
+ll.remove(4)
+ll.display()
--- python/stack.py
+++ python/stack.py
@@ -1,14 +1,26 @@
 class Stack:
   # write your __init__ method here that should store a 'total' value which is the total number of elements in the Stack and a 'stack' value which is an array of stored values in the Stack
+  def __init__(self):
+    self.stack = []
+    self.total = len(self.stack)
 
-  def push(self):
+  def push(self, data):
     # write your code to add data following LIFO and return the Stack
-    pass
+    self.stack.append(data)
 
-  def pop(self, data):
+  def pop(self):
     # write your code to removes the data following LIFO and return the Stack
-    pass
+    self.stack.pop()
 
   def size(self):
     # write your code that returns the size of the Stack
-    pass
+    return len(self.stack)
+
+
+# Testing
+x = Stack()
+x.push(5)
+x.push(2)
+print(x.size())
+x.pop()
+print(x.size())
